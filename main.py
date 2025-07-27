22e6e2eb2b40742e5d950bcf9cdf3f98e66f621e
iamaber <abirislam1892@gmail.com>
2025-07-27 16:29:32 +0600

    Added take_command and run_assistant functions

--- main.py
+++ main.py
@@ -23,4 +23,48 @@ def wish_user():
         speak("Good Afternoon!")
     else:
         speak("Good Evening!")
-    speak("I am your voice assistant. How can I help you today?")
+    speak("I am your voice assistant. How can I help you today?") 
+    
+def take_command():
+    return input("You (type your command): ").lower() 
+
+def run_assistant():
+    wish_user()
+    while True:
+        query = take_command()
+
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            try:
+                result = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia:")
+                speak(result)
+            except:
+                speak("Sorry, I couldn't find anything.")
+
+        elif 'open youtube' in query:
+            speak("Opening YouTube...")
+            webbrowser.open("https://www.youtube.com/")
+
+        elif 'open google' in query:
+            speak("Opening Google...")
+            webbrowser.open("https://www.google.com/")
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The current time is {strTime}")
+
+        elif 'joke' in query:
+            joke = pyjokes.get_joke()
+            speak(joke)
+
+        elif 'exit' in query or 'bye' in query:
+            speak("Goodbye! Have a nice day!")
+            break
+
+        else:
+            speak("Sorry, I didn't understand that. Try again.")
+            
+run_assistant()
+    
